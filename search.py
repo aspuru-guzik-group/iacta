8af72804e5f9f04ee0ff18f73c2c2fdfbd9e809f
Cyrille Lavigne <lavigne.cyrille@gmail.com>
2020-03-22 13:52:53 -0400

    Made xtb_run non-blocking / non-executing

--- search.py
+++ search.py
@@ -20,35 +20,53 @@ Chem.MolToMolFile(combined, "init_guess.mol")
 
 # Optimize
 opt = xtb.optimize("init_guess.mol", "optimized_guess.mol")
-combined = Chem.MolFromMolFile("optimized_guess.mol",
-                               removeHs=False)
 
-# Get the indices of the bond to stretch
-bond = get_bonds(combined, "CBr")[0]
+# combined = Chem.MolFromMolFile("optimized_guess.mol",
+#                                removeHs=False)
 
-# Start the run
-stretch_factors = 1.1**np.arange(10)
+# # Get the indices of the bond to stretch
+# bond = get_bonds(combined, "CBr")[0]
 
-import os
-import shutil
-os.makedirs("conformers", exist_ok=True)
-os.makedirs("stretch", exist_ok=True)
-MolToXYZFile(combined, "conformers/0.xyz")
-conformers = ["0.xyz"]
+# # Start the run
+# stretch_factors = 1.1**np.arange(10)
 
-stretch = stretch_factors[0]
-xcontrol = open("xcontrol", "w")
-xcontrol.write("$constrain\n"+
-               "  force constant = 0.5\n" +
-               "  distance: %i, %i, %f\n"% (bond[0],bond[1],
-                                            stretch * bond[2]) +
-               "$end")
-xcontrol.close()
+# import os
+# import shutil
+# os.makedirs("conformers", exist_ok=True)
+# os.makedirs("stretch", exist_ok=True)
+# MolToXYZFile(combined, "conformers/0.xyz")
+# conformers = ["0.xyz"]
 
-for c in conformers:
-    xtb.optimize("conformers/" + c, "stretch/" + c,
-                 xcontrol="xcontrol",
-                 log="stretch/" + c + ".log")
+# stretch = stretch_factors[0]
+# xcontrol = open("xcontrol", "w")
+# xcontrol.write("$constrain\n"+
+#                "  force constant = 0.5\n" +
+#                "  distance: %i, %i, %f\n"% (bond[0],bond[1],
+#                                             stretch * bond[2]) +
+#                "$end")
+# xcontrol.close()
 
+# for c in conformers:
+#     xtb.optimize("conformers/" + c, "stretch/" + c,
+#                  xcontrol="xcontrol",
+#                  log="stretch/" + c + ".log")
 
+# stretch = stretch_factors[0]
+# xcontrol = open("xcontrol", "w")
+# xcontrol.write("$constrain\n"+
+#                "  force constant = 0.5\n" +
+#                "  distance: %i, %i, %f\n"% (bond[0],bond[1],
+#                                             stretch * bond[2]) +
+#                "$end")
+# xcontrol.write("$metadyn\n"+
+#                "  save=10\n"+
+#                "  kpush=1.0\n"+
+#                "  alp=0.2\n"+
+#                "$end")
+# xcontrol.close()
+
+# metarun = xtb_utils.xtb_run("xtb",
+#                             "conformers/0.xyz",
+#                             "--metadyn",
+#                             xcontrol="xcontrol")
 
--- xtb_utils.py
+++ xtb_utils.py
@@ -5,12 +5,17 @@ import os
 import tempfile
 
 class xtb_run:
-    def __init__(self,xtb, geom_file, *args, cwd=".", xcontrol=None):
+    def __init__(self, xtb, geom_file,
+                 *args, cwd=".", xcontrol=None,
+                 return_files=[],
+                 block=True):
         self.dir = tempfile.mkdtemp(dir=cwd)
         self.out = open(self.dir + "/xtb.out", "w")
         self.err = open(self.dir + "/xtb.err", "w")
         self.coord = shutil.copy(geom_file, self.dir)
         self.cwd = cwd
+        self.return_files = return_files   # list of files to take out when
+                                           # run finishes
         
         self.args = [xtb]
         if xcontrol:
@@ -22,23 +27,52 @@ class xtb_run:
         self.args += args
         self.args += ["--", os.path.basename(self.coord)]
         
+        self.kwargs = dict(stderr=self.err,
+                           stdout=self.out,
+                           cwd=self.dir)
         
-        self.proc = subprocess.Popen(
-            self.args,
-            stderr=self.err,
-            stdout=self.out,
-            cwd=self.dir)
+        self.proc = None
+
+    def start(self, blocking=True):
+        assert self.proc is None
+        self.proc = subprocess.Popen(self.args, **self.kwargs)
+        if blocking:
+            self.proc.wait()
+
+    def assert_done(self):
+        if self.proc is None:
+            raise RuntimeError("Process not started")
+        
+        if self.proc.poll() is None:
+            raise RuntimeError("Process not finished")
 
     def cp(self, file_in, file_out=""):
         # Copy a file from xtb to file_out in the parent directory.
+        self.assert_done()
         return shutil.copy(self.dir + "/" + file_in,
-                    self.cwd + "/" + file_out)
-
+                           self.cwd + "/" + file_out)
 
     def close(self):
+        self.assert_done()
         self.out.close()
         self.err.close()
         shutil.rmtree(self.dir)
+
+    def __call__(self):
+        # Run xtb in a blocking manner, extract return files, close temporary
+        # directories.
+        self.start()
+        output = []
+        for file_in, file_out in self.return_files:
+            output += [self.cp(file_in,file_out)]
+
+        self.close()
+        
+        if len(output) == 1:
+            return output[0]
+        else:
+            return tuple(output)
+
 
 
 class xtb_driver:
@@ -50,14 +84,10 @@ class xtb_driver:
                  xcontrol=None,
                  log=None):
         file_ext = geom_file[-3:]
-        opt = xtb_run(self.xtb_bin, geom_file,
-                      "--opt", xcontrol=xcontrol,
-                      *self.extra_args)
-        opt.proc.wait()
-        out = opt.cp("xtbopt." + file_ext, out_file)
-        if log:
-            opt.cp("xtbopt.log", log)
-        opt.close()
-        return out 
+        opt = xtb_run(self.xtb_bin, geom_file, 
+                      "--opt", *self.extra_args,
+                      xcontrol=xcontrol,
+                      return_files=[("xtbopt." + file_ext, out_file)])
+        return opt
 
 
