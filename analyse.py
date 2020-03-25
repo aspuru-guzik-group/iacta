b37eeade2b85c6785c17770ede1202034a04b2c8
Cyrille Lavigne <lavigne.cyrille@gmail.com>
2020-03-25 10:39:18 -0400

    New analysis routines

--- analyse.py
+++ analyse.py
@@ -3,67 +3,26 @@ import cclib
 import glob
 
 def get_energies(files):
-    v = []
+    out = []
     for p in files:
+        v = []
         f = open(p, "r")
         for line in f:
             if line[1:5] == "ener":
                 v += [float(line[8:25])]
-    return v
 
-def get_smiles(files):
+        out += [v[:]]
+    return np.array(out)
+
+def get_smiles(file):
+    f = open(file, "r")
     out = []
-    for p in files:
-        f = open(p, "r")
-        for line in f:
-            out += [line.split("\t")[0]]
+    for line in f:
+        out += [line.rstrip()]
     return out
 
-# get energies
-mtd_indices = [5]
-energies = []
-# energies = [get_energies(["reactions/reactants_%2.2i.xyz"% m
-#                           for m in mtd_indices])]
-k = 0
-while True:
-    try:
-        new = get_energies(["reactions/prop%2.2i_%3.3i.xyz" % (m,k)
-                            for m in mtd_indices])
-    except FileNotFoundError:
-        break
-
-    energies += [new[:]]
-    k+=1
-
-energies += [get_energies(["reactions/products_%2.2i.xyz"% m
-                          for m in mtd_indices])]    
-energies = np.array(energies)
-
 # Get products and reactant smiles
-react_smi = get_smiles(["reactions/reactants_%2.2i.smi"% m
-                        for m in mtd_indices])
-prod_smi = get_smiles(["reactions/products_%2.2i.smi"% m
-                          for m in mtd_indices])
-
-reactions = {}
-dummies = []
-for i in range(len(react_smi)):
-    reactants = react_smi[i]
-    products = prod_smi[i]
-    path = energies[:,i]
-
-    if reactants == products:
-        dummies += [path]
-    else:
-        if (reactants, products) in reactions:
-            reactions[reactants,products] += [path]
-        else:
-            reactions[reactants,products] = [path]
-dummies = np.array(dummies)
-reaction = []
-paths = []
-for key, val in reactions.items():
-    reaction += [key]
-    paths += [np.array(val)]
-
+react_smi = get_smiles("mols/reactants.smi")
+prod_smi = get_smiles("mols/products.smi")
+energies = get_energies(glob.glob("mols/*.xyz"))
 
