6ad9add581c4d640605a8d78026cc0abd37b58a5
Cyrille Lavigne <lavigne.cyrille@gmail.com>
2020-03-28 11:36:15 -0400

    Made it into a commandline function

--- xtb2smiles.py
+++ xtb2smiles.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 from rdkit import Chem
 from rdkit_utils import fractional2bondtype
@@ -35,4 +36,16 @@ def xtb2smiles(xyzfile, sanitize=True):
         m = Chem.RemoveHs(m, sanitize=0)
     return Chem.MolToSmiles(m)
 
+if __name__ == "__main__":
+    import argparse
 
+    parser = argparse.ArgumentParser(description="Transform the output of an xtb calculation into a SMILES string.")
+    parser.add_argument("xyzfile", help="Path to the xyz file output by an xtb"+
+    "calculation. The directory containing xyzfile is assumed to contain wbo"+
+    "and charges xtb output files.")
+    parser.add_argument("-s",
+                        help="Flag to sanitize the RdKit molecule. Defaults to"+
+                        "false.",action="store_true")
+    
+    args = parser.parse_args()
+    print(xtb2smiles(args.xyzfile, sanitize=args.s))
