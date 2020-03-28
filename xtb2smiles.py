cd181c03f2e7269b3570a92ce51c3da20f5b53ed
Cyrille Lavigne <lavigne.cyrille@gmail.com>
2020-03-28 11:17:05 -0400

    Final form

--- xtb2smiles.py
+++ xtb2smiles.py
@@ -1,25 +1,35 @@
 import numpy as np
-from rdkit import Chem, Geometry
-from rdkit.Chem import AllChem, rdMolTransforms, rdmolops
-import os
+from rdkit import Chem
 from rdkit_utils import fractional2bondtype
 from xtb_utils import read_xtb_output
 
+def xtb2smiles(xyzfile):
+    """Transform the result of an xtb run into a smiles.
+
+    Parameters:
+    -----------
+    xyzfile (str) : path to the xyz file output by an xtb calculation. The
+    directory containing xyzfile is assumed to contain wbo and charges xtb
+    output files.
+
+    Returns:
+    --------
+    smiles (str) : The molecular SMILES, as a string.
+    """
+    atoms,charges,positions,wbo = read_xtb_output(xyzfile)
+    em = Chem.EditableMol(Chem.Mol())
+
+    for atom, charge in zip(atoms, charges):
+        new_atom = Chem.Atom(atom)
+        new_atom.SetFormalCharge(int(charge))
+        em.AddAtom(new_atom)
+
+    for i,j,fract_order in wbo:
+        em.AddBond(i,j,order=fractional2bondtype(fract_order))
+
+    m = em.GetMol()
+    m = Chem.RemoveHs(m)
+    Chem.SanitizeMol(m)
+    return Chem.MolToSmiles(m)
 
 
-atoms,charges,positions,wbo = read_xtb_output("test_xtb2smiles/xtbopt.xyz")
-em = Chem.EditableMol(Chem.Mol())
-
-for atom, charge in zip(atoms, charges):
-    new_atom = Chem.Atom(atom)
-    new_atom.SetFormalCharge(int(charge))
-    em.AddAtom(new_atom)
-
-for i,j,fract_order in wbo:
-    print(fract_order, fractional2bondtype(fract_order), atoms[i], atoms[j])
-    em.AddBond(i,j,order=fractional2bondtype(fract_order))
-    
-m = em.GetMol()
-m = Chem.RemoveHs(m)
-Chem.SanitizeMol(m)
-print(Chem.MolToSmiles(m))
