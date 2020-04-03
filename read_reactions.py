6d1958191f2d1cf921e6a5c85db011b4b8540651
Cyrille Lavigne <lavigne.cyrille@gmail.com>
2020-04-03 15:05:15 -0400

    Super improved output

--- read_reactions.py
+++ read_reactions.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob
 import constants
 import io_utils
+import os
 
 def xyz2smiles(xyzfile, chiral=False):
     output = []
@@ -47,22 +48,37 @@ def read_reaction(react_folder):
 
     imins = []
     for start,end in regions:
-        imins+= [np.argmin(E[start:end]) + start]
+        imin = np.argmin(E[start:end]) + start
+        stable = True
+        # Check if imin is a local minima
+        if imin < len(smiles)-1:
+            if E[imin]>E[imin+1]:
+                stable = False
+        if imin > 0:
+            if E[imin]>E[imin-1]:
+                stable = False
 
+        if stable:
+            imins += [imin]
+                
     # Important potential energy surface points
     ipots = [imins[0]]
+    # determine if these are stable, ts or errors
+    stable = [1]
     for k in range(1,len(imins)):
         # maximum between minima
         imax = np.argmax(E[imins[k-1]:imins[k]]) + imins[k-1]
         # if imax is different from both, we add it to the pot curve too
         if imax != imins[k] and imax != imins[k-1]:
             ipots += [imax]
+            stable += [0]
         ipots += [imins[k]]
+        stable += [1]
 
     energies = np.array([E[k] for k in ipots])
     smiles = [smiles[k] for k in ipots]
     structures = [(opt,k) for k in ipots]
-    return energies, smiles, structures
+    return energies, smiles, structures, stable
         
 def read_all_reactions(output_folder, verbose=True):
     """Read and parse all reactions in a given folder."""
@@ -77,16 +93,16 @@ def read_all_reactions(output_folder, verbose=True):
     
     for f in folders:
         try:
-            energies, smiles, structures = read_reaction(f)
+            energies, smiles, structures, stable = read_reaction(f)
         except OSError:
             # Convergence failed
             failed += [f]
-
-        if len(smiles)>1:
-            pathways += [(energies, smiles, structures)]
         else:
-            # No reaction in this pathway
-            noreact += [f]
+            if len(smiles)>1:
+                pathways += [(energies, smiles, structures, stable)]
+            else:
+                # No reaction in this pathway
+                noreact += [f]
 
             
     if verbose:
@@ -94,8 +110,38 @@ def read_all_reactions(output_folder, verbose=True):
         print(" - %6i with no reactions" % len(noreact))
         print("--------------")
         print(" = %6i reactions found" % len(pathways))
-        # print("    with %6i distinct stable species" % len(species))
     return pathways
+
+def prune_pathways(pathways):
+    reactions = {}
+
+    for energies, species, files, stable in pathways:
+        stables = []
+        barriers = []
+        current = 0.0
+        for i in range(len(species)):
+            if stable[i]:
+                stables += [species[i]]
+                barriers += [current]
+                estable = energies[i]
+                current = 0.0
+            else:
+                barrier = energies[i] - estable
+                if barrier > current:
+                    current = barrier
+        barriers = np.array(barriers[1:])
+        stables = tuple(stables)
+
+        if stables in reactions:
+            if (np.max(barriers)< np.max(reactions[stables][0])):
+                # if all the barriers are smaller, we make this the new best
+                # reaction.
+                reactions[stables] = (barriers, energies, species, files, stable)
+        else:
+            reactions[stables] = (barriers, energies, species, files, stable)
+
+    return reactions
+
 
 
 if __name__ == "__main__":
@@ -103,8 +149,76 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Read reaction products."
         )
-
     parser.add_argument("folder", help="Folder containing the react*** files.")
+    parser.add_argument("-o", help="Folder where final results are stored.",
+                        default=None)
     args = parser.parse_args()
     folder =args.folder
+    outfolder = args.o
+    if outfolder is None:
+        outfolder = folder + "/results"
+    os.makedirs(outfolder, exist_ok=True)
+    
     pathways = read_all_reactions(folder)
+    print("     ...pruning identical reaction pathways...")
+    reactions = prune_pathways(pathways)
+    print("   %6i reaction pathways are unique." % len(reactions))
+    print("printing...")
+    print("\n\n")
+    hartree_ev = 27.2113860217
+    kcalmol_ev = 23.061
+    mol_index = {}
+    index = 1
+    for key in reactions.keys():
+        for molecule in key:
+            if molecule not in mol_index.keys():
+                mol_index[molecule] = index
+                mol = pybel.readstring("smi", molecule)
+                # save to file
+                mol.draw(show=False,
+                         filename=outfolder + "/mol%3.3i.png"%index)
+                index += 1
+
+
+    start = "|--=====-----==-- REACTIONS AND BARRIERS (kcal/mol) --==------====---|\n"
+    start+= "File    Reaction\n"
+    index = 1
+    f = open(outfolder + "/summary", "w")
+    print(start, end="")
+    f.write(start)
+    for key, val in reactions.items():
+        upper = "%3i >   " % index +  key[0]
+        for i in range(1,len(key)):
+            new = " == %.2f ==> " % (val[0][i-1]*hartree_ev * kcalmol_ev) + key[i]
+            upper += new
+        upper += "\n"
+
+        print(upper, end="")
+        f.write(upper)
+
+        with open(outfolder + "/reactions%i.xyz" % index, "w") as ftraj:
+            for file, ind in val[3]:
+                xyz = io_utils.read_trajectory(file, ind)
+                ftraj.write(xyz)
+        index += 1
+
+    table = ("\n\nTable of molecules\n"
+              +  "------------------\n")
+    for smile, index in mol_index.items():
+        table += "mol%3.3i.png\t"% index + smile + "\n"
+
+    print(table)
+    f.write(table)
+        
+    f.close()
+    
+
+        
+    
+                    
+        
+
+        
+
+            
+
