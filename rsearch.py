c4ddea040c06d2be5df4867efc3225c13a0b0eea
Cyrille Lavigne <lavigne.cyrille@gmail.com>
2020-04-11 11:11:40 -0400

    very good progress!

--- rsearch.py
+++ rsearch.py
@@ -8,6 +8,82 @@ import argparse
 from constants import hartree_ev, ev_kcalmol
 import yaml
 
+def rsearch(out_dir, log_level=0, nthreads=1):
+    if "LOCALSCRATCH" in os.environ:
+        scratch = os.environ["LOCALSCRATCH"]
+    else:
+        print("warning: $LOCALSCRATCH not set")
+        scratch = "."
+
+    # load parameters
+    with open(out_dir + "/user.yaml", "r") as f:
+        user_params = yaml.load(f, Loader=yaml.Loader)
+    with open(out_dir + "/default.yaml", "r") as f:
+        params = yaml.load(f, Loader=yaml.Loader)        
+
+    # Merge, replacing defaults with user parameters
+    for key,val in user_params.items():
+        params[key] = val
+
+    # Interpret log level
+    if log_level>1:
+        delete=False
+    else:
+        delete=True
+
+    # Command log file
+    if log_level >0:
+        logfile = open(out_dir + "/commandlog", "a")
+        logfile.write("--------------------------"
+                      +"--------------------------------------\n")
+    else:
+        logfile = None
+
+
+
+    # Initialize the xtb driver
+    # -------------------------
+    xtb = xtb_utils.xtb_driver(scratch=scratch,
+                               delete=delete,
+                               logfile=logfile)
+
+    xtb.extra_args = ["--gfn",params["gfn"]]
+    if params["etemp"]:
+        xtb.extra_args += ["--etemp", params["etemp"]]
+    if params["chrg"]:
+        xtb.extra_args += ["--chrg", params["chrg"]]
+    if params["uhf"]:
+        xtb.extra_args += ["--uhf", params["uhf"]]    
+    if params["solvent"]:
+        xtb.extra_args += ["--gbsa", params["solvent"]]
+        
+    # Temporarily set -P to number of threads for the next, non-parallelizable
+    # two steps.
+    xtb.extra_args += ["-P", str(nthreads)]
+
+    # Optimize starting geometry including wall
+    # -----------------------------------------
+    init1 = out_dir + "/init.xyz"
+    with open(init1, "w") as f:
+        f.write(params["xyz"])
+    
+
+    print("Optimizing initial geometry 📐...")
+    opt = xtb.optimize(init1, init1,
+                       level=params["optim"],
+                       xcontrol={"wall":params["wall"]})
+    opt()
+
+    # Read result of optimization
+    atoms, positions, E = io_utils.traj2npy(init1, index=0)
+    print("    E₀    = %15.7f Eₕ" % E)
+    Emax = E + params["ewin"] / (hartree_ev * ev_kcalmol)
+    print("    max E = %15.7f Eₕ  (E₀ + %5.1f kcal/mol)" %
+          (Emax,params["threshold"]))
+     params["emax"] = Emax           # update parameters
+        
+    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Simple driver for reaction search. Builds a parameter file in an output folder.",
@@ -75,7 +151,7 @@ if __name__ == "__main__":
     # Get default parameters
     params_file = args.params
     with open(params_file, "r") as f:
-        default_params = yaml.load(f)
+        default_params = yaml.load(f, Loader=yaml.Loader)
 
     # Save user-set parameters for reproducibility.
     user_params = {}
@@ -101,106 +177,30 @@ if __name__ == "__main__":
     # Additionally save defaults for reproducibility
     shutil.copy(params_file, out_dir)
 
+    rsearch(out_dir, log_level=args.log_level,
+            nthreads=args.threads)
     
-"""
-if "LOCALSCRATCH" in os.environ:
-    scratch = os.environ["LOCALSCRATCH"]
-else:
-    print("warning: $LOCALSCRATCH not set")
-    scratch = "."
+"""    
 
-# Interpret log level
-if args.log_level>1:
-    delete=False
-else:
-    delete=True
-
-
-
-
-
-if do_opt:
-    init0 = shutil.copy(args.init_xyz, out_dir + "/initial_geometry.xyz")
-else:
-    init0 = out_dir + "/initial_geometry.xyz"
-
-# Command log file
-if args.log_level >0:
-    logfile = open(out_dir + "/commandlog", "a")
-    logfile.write("--------------------------"
-                  +"--------------------------------------\n")
-else:
-    logfile = None
-    
-atoms, positions, E0 = io_utils.traj2npy(init0, index=0)
-Natoms = len(atoms)
-
-# Initialize the xtb driver
-# -------------------------
-xtb = xtb_utils.xtb_driver(scratch=scratch,
-                           delete=delete,
-                           logfile=logfile)
-xtb.extra_args = ["--gfn",args.gfn]
-if args.etemp:
-    xtb.extra_args += ["--etemp", args.etemp]
-if args.chrg:
-    xtb.extra_args += ["--chrg", args.chrg]
-if args.uhf:
-    xtb.extra_args += ["--uhf", args.uhf]    
-if args.solvent:
-    xtb.extra_args += ["--gbsa", args.solvent]
-
-# Initialize parameters
-# ---------------------
-with open(args.params, "r") as f:
-    params = yaml.load(f)
-
-ethreshold = args.threshold / (hartree_ev * ev_kcalmol)
-params = react.default_parameters(Natoms,
-                                  shake=args.shake_level,
-                                  ethreshold=ethreshold,
-                                  optlevel=args.opt_level,
-                                  log_level=args.log_level)
-
-# Temporarily set -P to number of threads for the next, non-parallelizable two
-# steps.
-xtb.extra_args += ["-P", str(args.T)]
-
-# Optimize starting geometry including wall
-# -----------------------------------------
-init1 = out_dir + "/initial_optimized.xyz"
-if do_opt:
-    print("Optimizing initial geometry 📐...")
-    opt = xtb.optimize(init0, init1,
-                       level=args.opt_level,
-                       xcontrol={"wall":params["wall"]})
-    opt()
-
-# Read result of optimization
-atoms, positions, E = io_utils.traj2npy(init1, index=0)
-Emax = E + ethreshold
-print("    E₀    = %15.7f Eₕ" % E)
-print("    max E = %15.7f Eₕ  (E₀ + %5.1f kcal/mol)" % (Emax,args.threshold))
-params["emax"] = Emax           # update parameters
 
 
 # Get bond parameters
 # -------------------
-bond_length0 = np.sqrt(np.sum((positions[args.atoms[0]-1] -
-                               positions[args.atoms[1]-1])**2))
-bond = (args.atoms[0], args.atoms[1], bond_length0)
+bond_length0 = np.sqrt(np.sum((positions[params["atoms"][0]-1] -
+                               positions[params["atoms"][1]-1])**2))
+bond = (params["atoms"][0], params["atoms"][1], bond_length0)
 
 # Constraints for the search
 # -------------------------
-stretch_factors = np.linspace(args.s[0], args.s[1], args.sn)
+stretch_factors = np.linspace(params["s"][0], params["s"][1], params["sn"])
 print("Stretching bond between atoms %s%i and %s%i"
       %(atoms[bond[0]-1], bond[0], atoms[bond[1]-1], bond[1]))
-print("    with force constant 💪💪 %f" % args.force)
+print("    with force constant 💪💪 %f" % params["force"])
 print("    between 📏 %7.2f and %7.2f A (%4.2f to %4.2f x bond length)"
       % (min(stretch_factors)*bond[2], max(stretch_factors)*bond[2],
          min(stretch_factors), max(stretch_factors)))
 print("    discretized with %i points" % len(stretch_factors))
-constraints = [("force constant = %f" % args.force,
+constraints = [("force constant = %f" % params["force"],
                 "distance: %i, %i, %f"% (bond[0],bond[1],
                                          stretch * bond[2]))
                for stretch in stretch_factors]
@@ -217,7 +217,7 @@ if do_stretch:
 # reset threading
 xtb.extra_args = xtb.extra_args[:-2]
 
-mtd_indices = args.mtdi
+mtd_indices = params["mtdi"]
 if mtd_indices is None:
     # Read the successive optimization, then set mtd points to ground and TS
     # geometries.
