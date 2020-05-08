6bd1874f4773955fe0788bab3900a9612b87eb88
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2020-05-08 19:14:56 -0400

    added examples submit scripts

---
+++ submit_restart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -A rrg-aspuru
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=8:00:00
+#SBATCH --job-name minigabe-restart
+
+module load intel
+module load python/3.6.4-anaconda5.1.0
+export OMP_NUM_THREADS=1 MKL_NUM_THREADS=1
+export PATH=$HOME/xtb/bin:$PATH
+export LOCALSCRATCH=$SLURM_TMPDIR
+export MINIGABE=$HOME/ts-search
+
+source activate ts-search
+python $MINIGABE/rsearch-restart.py -t 40 $@
