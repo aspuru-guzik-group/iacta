61224eb1e8b8d101ea6a8e72f2d565d1a23fb6e3
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2020-07-03 12:35:55 -0400

    updated scripts to include NiaEnv, python3.7

--- scinet_setup_env.sh
+++ scinet_setup_env.sh
@@ -4,13 +4,14 @@
 pushd . > /dev/null
 SCRIPT_PATH="${BASH_SOURCE[0]}"
 if ([ -h "${SCRIPT_PATH}" ]); then
-	while([ -h "${SCRIPT_PATH}" ]); do cd `dirname "$SCRIPT_PATH"`; 
-		SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+    while([ -h "${SCRIPT_PATH}" ]); do cd `dirname "$SCRIPT_PATH"`;
+        SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
 fi
 cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 
+module load NiaEnv/2018a
 module load intel
 module load python/3.6.4-anaconda5.1.0
 export OMP_NUM_THREADS=1 MKL_NUM_THREADS=1
