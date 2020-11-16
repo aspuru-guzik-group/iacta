1074b14f07345de08614f8f320b501701dfc5824
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2020-11-16 13:02:09 -0500

    fixed submit scripts and added environment.yml

--- submit_restart.sh
+++ submit_restart.sh
@@ -5,9 +5,8 @@
 #SBATCH --time=8:00:00
 #SBATCH --job-name minigabe-restart
 
-module load NiaEnv/2018a
-module load intel
-module load python/3.6.4-anaconda5.1.0
+module load NiaEnv
+module load conda3
 export OMP_NUM_THREADS=1 MKL_NUM_THREADS=1
 export PATH=$HOME/xtb/bin:$PATH
 export LOCALSCRATCH=$SLURM_TMPDIR
